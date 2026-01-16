a9a7acc4779d6e74a52ad70e3ccf6bc45bf83a57
Mayank <mm884209@gmail.com>
2026-01-17 03:11:24 +0530

    refactor: modify migration for player and match table

--- db/migrate/20260116213547_create_players.rb
+++ db/migrate/20260116213547_create_players.rb
@@ -1,9 +1,9 @@
 class CreatePlayers < ActiveRecord::Migration[8.1]
   def change
     create_table :players do |t|
-      t.string :name
-      t.integer :wins
-      t.integer :losses
+      t.string  :name, null: false
+      t.integer :wins, default: 0, null: false
+      t.integer :losses, default: 0, null: false
 
       t.timestamps
     end
--- db/migrate/20260116213558_create_matches.rb
+++ db/migrate/20260116213558_create_matches.rb
@@ -1,10 +1,9 @@
 class CreateMatches < ActiveRecord::Migration[8.1]
   def change
     create_table :matches do |t|
-      t.references :winner, null: false, foreign_key: true
-      t.references :loser, null: false, foreign_key: true
-      t.datetime :played_at
-
+      t.references :winner, null: false, foreign_key: { to_table: :players }
+      t.references :loser,  null: false, foreign_key: { to_table: :players }
+      t.datetime :played_at, default: -> { "CURRENT_TIMESTAMP" }
       t.timestamps
     end
   end
