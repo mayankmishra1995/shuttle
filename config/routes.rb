a836f19fb9e3934ca5b16ca0d417231218322868
Mayank <mm884209@gmail.com>
2026-01-17 20:38:03 +0530

    feat: add schema and controllers for players, matches, and leaderboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get "leaderboard/index"
+  get "matches/new"
+  get "matches/create"
+  get "players/index"
+  get "players/create"
+  get "players/destroy"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
