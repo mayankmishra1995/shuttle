0710563ec645873560694d371a649ea4573a6dc9
Mayank <mm884209@gmail.com>
2026-01-17 21:04:21 +0530

    feat: add seed data for players

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+["Alex", "Sam", "Jordan", "Chris"].each do |name|
+  Player.find_or_create_by!(name: name)
+end
+  
