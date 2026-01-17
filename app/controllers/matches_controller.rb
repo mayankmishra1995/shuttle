2c4e1470a2dd965bfccb51016d1cc06272ba3dc5
Mayank <mm884209@gmail.com>
2026-01-17 20:45:45 +0530

    feat: add crud to the matches controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,7 +1,21 @@
 class MatchesController < ApplicationController
   def new
+    @players = Player.all
   end
 
   def create
+    match = Match.new(match_params)
+
+    if match.save
+      redirect_to leaderboard_path, notice: "Match recorded"
+    else
+      redirect_to new_match_path, alert: match.errors.full_messages.join(", ")
+    end
+  end
+
+  private
+
+  def match_params
+    params.require(:match).permit(:winner_id, :loser_id)
   end
 end
