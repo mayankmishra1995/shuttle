9085f8b20ff11af0063cf0a42000c7f76ed2cf85
Mayank <mm884209@gmail.com>
2026-01-17 20:45:11 +0530

    feat: add crud to the players controller

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,10 +1,26 @@
 class PlayersController < ApplicationController
   def index
+    @players = Player.all
   end
 
   def create
+    player = Player.new(player_params)
+    if player.save
+      redirect_to players_path, notice: "Player added"
+    else
+      redirect_to players_path, alert: player.errors.full_messages.join(", ")
+    end
   end
 
   def destroy
+    player = Player.find(params[:id])
+    player.destroy
+    redirect_to players_path, notice: "Player removed"
+  end
+
+  private
+
+  def player_params
+    params.require(:player).permit(:name)
   end
 end
